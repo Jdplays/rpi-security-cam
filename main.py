6d5ffdab7c0732e96daa613da92dd80ca09de73d
James Dougan <jdplaysgames200@gmail.com>
2023-10-21 00:34:00 +0100

    obj

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
+        frame = camera.get_object()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')  # Use frame directly
 
